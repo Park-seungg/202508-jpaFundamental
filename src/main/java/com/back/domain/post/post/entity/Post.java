c14bd96de59351b3ad18b9d61965e97b8ffbe6ba
Park-seungg <parkis1101@gmail.com>
2025-08-21 10:23:24 +0900

    final 키워드 실험

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -2,23 +2,31 @@ package com.back.domain.post.post.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@NoArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
-@Entity
+@Entity // 아래 구조대로 DB 테이블(컬럼)을 만들어야 한다.
+@ToString
 public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String title;
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private final int id; // INT
+    private final String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
-    private String content;
+    private final String content; // TEXT
 
-    public Post(String title, String content) {
+
+    public Post (String title, String content) {
+        this.id = 0;
         this.title = title;
         this.content = content;
+    }
+
+    public Post() {
+        this("", "");
     }
 }
