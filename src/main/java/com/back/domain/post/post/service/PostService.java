ca93fb511b755962a33b33a8f919b8f95f28c351
Park-seungg <parkis1101@gmail.com>
2025-08-22 13:00:20 +0900

    @ManyToOne Member author; 추가 및 관련 코드 수정

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.service;
 
+import com.back.domain.member.member.entity.Member;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -33,8 +34,8 @@ public class PostService {
         // UPDATE post SET title = '제목 1', content = '내용 1', modify_date = '2025-08-22 10:19:31' WHERE id = 1;
     }
 
-    public Post write(String title, String content) {
-        Post post = new Post(title, content);
+    public Post write(Member author, String title, String content) {
+        Post post = new Post(author, title, content);
 
         return postRepository.save(post);
     }
